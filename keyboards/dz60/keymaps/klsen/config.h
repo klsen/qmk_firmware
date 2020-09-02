3ac11d9930306a9ea2bc06ca571dba026ab5135f
Kenneth Seneres <klseneres@gmail.com>
2020-09-02 16:35:01 -0400

    rearranged lines

--- keyboards/dz60/keymaps/klsen/config.h
+++ keyboards/dz60/keymaps/klsen/config.h
@@ -1,7 +1,7 @@
-// hope i'm not breaking anything
 #define RGBLIGHT_LAYERS
 #define GRAVE_ESC_CTRL_OVERRIDE
 
+// hope i'm not breaking anything
 //#define NO_SUSPEND_POWER_DOWN
 //#pragma once
-//#undef RGBLIGHT_SLEEP 
+//#undef RGBLIGHT_SLEEP
