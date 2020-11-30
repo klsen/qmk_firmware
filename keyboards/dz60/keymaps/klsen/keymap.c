bc62fe408ff39af0b94be49aaf8c6b3657f9d7c4
Kenneth Seneres <klseneres@gmail.com>
2020-11-30 12:35:00 -0500

    new feature where fn+backspace2 swaps spacebar and backspace for righties. reverted numpad feature to number row keys instead of numpad keys for compatibility without needing a numlock button

--- keyboards/dz60/keymaps/klsen/keymap.c
+++ keyboards/dz60/keymaps/klsen/keymap.c
@@ -20,28 +20,48 @@ enum my_keycodes {
 	KC_BSPC2 = SAFE_RANGE,
 	RGB_ALT,
 	RGB_TWKL,
-	WPM_MODE
+	WPM_MODE,
+    SWP_SPC
 };
 
 enum my_layers {
 	_base,
 	_fn,
+    _base2,
+    _fn2,
 	_rgb
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // primary layout
 	[_base] = LAYOUT_all( \
 		KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_NO  , KC_BSPC2, \
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, \
 		KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
 		KC_LSFT, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , MO(_rgb), \
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , MO(_fn), KC_BSPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
-	[_fn] = LAYOUT_all( \
-		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , _______, \
-		_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_P7  , KC_P8  , KC_P9  , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_P4  , KC_P5  , KC_P6  , KC_PPLS, KC_PMNS, KC_NO  , \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_P0  , KC_P1  , KC_P2  , KC_P3  , KC_NO  , _______, KC_PGUP, _______, \
+	// secondary layout for alternate functions
+    [_fn] = LAYOUT_all( \
+		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
+		_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
+		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
+		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_P3  , KC_NO  , _______, KC_PGUP, _______, \
 		_______, _______, _______, _______, KC_TRNS, KC_DEL , _______, _______, KC_HOME, KC_PGDN, KC_END),
+    // primary layout with space and backspace flipped for righties
+    [_base2] = LAYOUT_all( \
+		KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_NO  , KC_BSPC2, \
+		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, \
+		KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
+		KC_LSFT, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , MO(_rgb), \
+		KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, MO(_fn2), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+    // secondary layout with del moved for righties
+	[_fn2] = LAYOUT_all( \
+		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
+		_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
+		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
+		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_P3  , KC_NO  , _______, KC_PGUP, _______, \
+		_______, _______, _______, KC_DEL , KC_TRNS, _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
+    // layout for setting rgb
 	[_rgb] = LAYOUT_all( \
 		RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, RGB_ALT, RGB_TWKL,KC_NO  , KC_NO , KC_NO  , \
 		KC_NO  , KC_NO  , WPM_MODE,KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RGB_RMOD,RGB_MOD, KC_NO , \
@@ -111,6 +131,7 @@ bool led_update_user(led_t led_state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	static bool wpm_mode_active = false;
+    static bool swap_space = false;
 	uint8_t prev_mode = rgblight_get_mode();
 	switch (keycode) {
 		// custom keycodes for lighting tricks
@@ -132,6 +153,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				rgblight_sethsv(hue, sat, val);
 			}
 			return true;
+        // code to swap functionality of spacebar and backspace keys on a toggle.
+        // using custom keycode instead of DF() keycode provided so I can turn on some lights
+        case SWP_SPC:
+            if (record->event.pressed) {
+                swap_space = !swap_space;
+                mode = rgblight_get_mode();
+				hue = rgblight_get_hue();
+				sat = rgblight_get_sat();
+				val = rgblight_get_val();
+                if (mode == RGBLIGHT_MODE_STATIC_LIGHT) {
+                    rgblight_sethsv_at(hue, sat, 255, 7);
+                }
+                if (swap_space == true) default_layer_set(1U << _base2);
+                else default_layer_set(1U << _base);
+            }
+            else {
+                rgblight_sethsv_at(hue, sat, val, 7);
+            }
+            return true;
         // keycode for alternating lighting mode. didn't have its own keycode before.
 		case RGB_ALT:
 			if (record->event.pressed) {
