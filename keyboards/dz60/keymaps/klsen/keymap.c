42fb325eef04bfddeab5e3d354e20b5c4a97ad18
Kenneth Seneres <klseneres@gmail.com>
2021-02-08 14:22:58 -0500

    cleanup

--- keyboards/dz60/keymaps/klsen/keymap.c
+++ keyboards/dz60/keymaps/klsen/keymap.c
@@ -28,8 +28,6 @@ enum my_keycodes {
 enum my_layers {
 	_base,
 	_fn,
-    _base2,
-    _fn2,
 	_rgb
 };
 
@@ -48,20 +46,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
 		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_3   , KC_NO  , _______, KC_PGUP, _______, \
 		_______, _______, _______, _______, KC_TRNS, KC_DEL , _______, _______, KC_HOME, KC_PGDN, KC_END),
-    // primary layout with space and backspace flipped for righties
-    [_base2] = LAYOUT_all( \
-		KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_NO  , KC_BSPC2, \
-		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, \
-		KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
-		KC_LSFT, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , MO(_rgb), \
-		KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, MO(_fn2), KC_SPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
-    // secondary layout with del moved for righties
-	[_fn2] = LAYOUT_all( \
-		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
-		_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_3   , KC_NO  , _______, KC_PGUP, _______, \
-		_______, _______, _______, KC_DEL , KC_TRNS, _______, _______, _______, KC_HOME, KC_PGDN, KC_END),
     // layout for setting rgb
 	[_rgb] = LAYOUT_all( \
 		RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, RGB_ALT, RGB_TWKL,KC_NO  , KC_NO , KC_NO  , \
@@ -89,11 +73,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     if (rgblight_get_mode() == RGBLIGHT_MODE_STATIC_LIGHT){
         switch(layer) {
             case _base:
-            case _base2:
                 if (prev_layer != _rgb) rgblight_mode(mode);
                 break;
             case _fn:
-            case _fn2:
                 mode = rgblight_get_mode();
                 rgblight_sethsv_range(hue, sat, 180, 8, 16);
                 break;
@@ -170,8 +152,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 if (mode == RGBLIGHT_MODE_STATIC_LIGHT) {
                     rgblight_sethsv_at(hue, sat, 255, 7);
                 }
-                // if (swap_space == true) default_layer_set(1U << _base2);
-                // else default_layer_set(1U << _base);
             }
             else {
                 rgblight_sethsv_at(hue, sat, val, 7);
