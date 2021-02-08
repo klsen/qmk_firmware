72b86757dc5c4585685945204d1d6d277d60f9dd
Kenneth Seneres <klseneres@gmail.com>
2021-02-08 15:38:14 -0500

    now i got a mouse layer on toggle. mouse movement on wasd, scroll with fn and wasd, constant acceleration modified with jkl. also moved media keys to fn wasd

--- keyboards/dz60/keymaps/klsen/config.h
+++ keyboards/dz60/keymaps/klsen/config.h
@@ -1,6 +1,18 @@
 #define RGBLIGHT_LAYERS
 #define GRAVE_ESC_CTRL_OVERRIDE
 
+// mouse keys settings
+#define MK_3_SPEED
+#define MK_C_OFFSET_0 3
+#define MK_C_OFFSET_1 6
+#define MK_C_OFFSET_2 10
+#define MK_C_INTERVAL_0 8
+#define MK_C_INTERVAL_1 8
+#define MK_C_INTERVAL_2 8
+#define MK_W_INTERVAL_0 160
+#define MK_W_INTERVAL_1 80
+#define MK_W_INTERVAL_2 40
+
 // hope i'm not breaking anything
 //#define NO_SUSPEND_POWER_DOWN
 //#pragma once
--- keyboards/dz60/keymaps/klsen/keymap.c
+++ keyboards/dz60/keymaps/klsen/keymap.c
@@ -28,7 +28,8 @@ enum my_keycodes {
 enum my_layers {
 	_base,
 	_fn,
-    _fn2,
+    _mouse,
+    _mouse_mod,
 	_rgb
 };
 
@@ -37,22 +38,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_base] = LAYOUT_all( \
 		KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_NO  , KC_BSPC2, \
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, \
-		MO(_fn2), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
+		TG(_mouse), KC_A, KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
 		KC_LSFT, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , MO(_rgb), \
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , MO(_fn), KC_BSPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 	// secondary layout for alternate functions
     [_fn] = LAYOUT_all( \
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
+		_______, KC_MUTE, KC_VOLU, KC_MPLY, KC_NO  , KC_NO  , KC_NO  , KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
+		_______, KC_MPRV, KC_VOLD, KC_MNXT, KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
 		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_3   , KC_NO  , _______, KC_PGUP, _______, \
 		_______, _______, _______, _______, KC_TRNS, KC_DEL , _______, _______, KC_HOME, KC_PGDN, KC_END),
-    [_fn2] = LAYOUT_all( \
-		KC_NO  , KC_VOLD  , KC_VOLU  , KC_MPRV  , KC_MPLY  , KC_MNXT  , KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
-		_______, KC_MPRV, KC_WH_U, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
-		_______, KC_WH_L, KC_WH_D, KC_WH_R  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
-		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_3   , KC_NO  , _______, KC_PGUP, _______, \
-		_______, _______, _______, _______, KC_TRNS, KC_DEL , _______, _______, KC_HOME, KC_PGDN, KC_END),
+    [_mouse] = LAYOUT_all( \
+		KC_NO  , KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , \
+		KC_NO  , KC_BTN2, KC_MS_U, KC_BTN1, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , \
+		_______, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO  , KC_NO  , KC_NO  , KC_ACL0, KC_ACL1, KC_ACL2, KC_NO  , KC_NO  , KC_NO  , \
+		KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , _______, \
+		KC_NO  , KC_NO  , KC_NO  , KC_NO  , MO(_mouse_mod),KC_NO, KC_NO, KC_NO, KC_NO  , KC_NO  , KC_NO  ),
+    [_mouse_mod] = LAYOUT_all( \
+		KC_NO  , KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , \
+		KC_NO  , _______, KC_WH_U, _______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , \
+		_______, KC_WH_L, KC_WH_D, KC_WH_R, KC_NO  , KC_NO  , KC_NO  , _______, _______, _______, KC_NO  , KC_NO  , KC_NO  , \
+		KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , _______, \
+		KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_TRNS, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ),
     // layout for setting rgb
 	[_rgb] = LAYOUT_all( \
 		RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, RGB_ALT, RGB_TWKL,KC_NO  , KC_NO , KC_NO  , \
