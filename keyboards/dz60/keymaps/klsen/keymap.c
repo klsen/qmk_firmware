c10db6900b0be342cbd9899fa08117f59a5e1836
Kenneth Seneres <klseneres@gmail.com>
2021-02-08 14:40:37 -0500

    mouse wheel scrolling with wasd and old capslock key. just testing if it works

--- keyboards/dz60/keymaps/klsen/keymap.c
+++ keyboards/dz60/keymaps/klsen/keymap.c
@@ -28,6 +28,7 @@ enum my_keycodes {
 enum my_layers {
 	_base,
 	_fn,
+    _fn2,
 	_rgb
 };
 
@@ -36,14 +37,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_base] = LAYOUT_all( \
 		KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_NO  , KC_BSPC2, \
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, \
-		KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
+		MO(_fn2), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , \
 		KC_LSFT, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_UP  , MO(_rgb), \
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC , MO(_fn), KC_BSPC, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 	// secondary layout for alternate functions
     [_fn] = LAYOUT_all( \
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
-		_______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
+		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
 		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
+		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_3   , KC_NO  , _______, KC_PGUP, _______, \
+		_______, _______, _______, _______, KC_TRNS, KC_DEL , _______, _______, KC_HOME, KC_PGDN, KC_END),
+    [_fn2] = LAYOUT_all( \
+		KC_NO  , KC_VOLD  , KC_VOLU  , KC_MPRV  , KC_MPLY  , KC_MNXT  , KC_NO  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_NO  , SWP_SPC, \
+		_______, KC_MPRV, KC_WH_U, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, KC_7   , KC_8   , KC_9   , KC_PAST, KC_PSLS, KC_NO  , KC_CALC, \
+		_______, KC_WH_L, KC_WH_D, KC_WH_R  , KC_NO  , KC_NO  , KC_NO  , KC_4   , KC_5   , KC_6   , KC_PPLS, KC_PMNS, KC_NO  , \
 		_______, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_0   , KC_1   , KC_2   , KC_3   , KC_NO  , _______, KC_PGUP, _______, \
 		_______, _______, _______, _______, KC_TRNS, KC_DEL , _______, _______, KC_HOME, KC_PGDN, KC_END),
     // layout for setting rgb
@@ -224,6 +231,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				else if (wpm >= 150) rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE+4);
 				else rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE);
 			}
+            // code for handling space and backspace swap
             if (swap_space && (keycode == KC_SPC || keycode == KC_BSPC || keycode == KC_DEL)) {
                 if (record->event.pressed) {
                     if (keycode == KC_SPC) {
