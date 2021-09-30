dd41aa5440672279568184592e8c4d3a31bfeee4
klsen <klseneres@gmail.com>
2021-09-29 22:38:27 -0400

    bug fixes for dz60 board

--- keyboards/dz60/keymaps/klsen/config.h
+++ keyboards/dz60/keymaps/klsen/config.h
@@ -4,17 +4,17 @@
 // mouse keys settings
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
-#define MK_C_OFFSET_0 3
+#define MK_C_OFFSET_0 2
 #define MK_C_OFFSET_1 6
-#define MK_C_OFFSET_2 10
+#define MK_C_OFFSET_2 18
 #define MK_C_OFFSET_UNMOD 6
 #define MK_C_INTERVAL_0 8
 #define MK_C_INTERVAL_1 8
 #define MK_C_INTERVAL_2 8
 #define MK_C_INTERVAL_UNMOD 8
 #define MK_W_INTERVAL_0 160
-#define MK_W_INTERVAL_1 80
-#define MK_W_INTERVAL_2 40
+#define MK_W_INTERVAL_1 100
+#define MK_W_INTERVAL_2 30
 
 #define COMBO_COUNT 1
 #define COMBO_TERM 50
